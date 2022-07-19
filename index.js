9bc3b9a42c435fbc93d3614d6fdbb252f35a1697
meghrajclg <109586518+meghrajclg@users.noreply.github.com>
2022-07-19 15:00:40 +0530

    auth

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-var a = 10;
-console.log(a)
+validateonSubmit() {
+    let self = this; // setup calls to the "this" values of the class described in the constructor
+
+    // add a "submit" event listener to the form
+    this.form.addEventListener("submit", (e) => {
+        // remove default functionality 
+        e.preventDefault();
+        var error = 0;
+        // loop through the fields and check them against a function for validation
+        self.fields.forEach((field) => {
+            const input = document.querySelector(`#${field}`);
+            if (self.validateFields(input) == false) {
+                // if a field does not validate, auto-increment our error integer
+                error++;
+            }
+        });
+        // if everything validates, error will be 0 and can continue
+        if (error == 0) {
+            //do login api here or in this case, just submit the form and set a localStorage item
+            localStorage.setItem("auth", 1);
+            this.form.submit();
+        }
+    });
+}
